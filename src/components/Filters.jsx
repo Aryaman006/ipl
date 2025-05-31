fcfb486db898f3000c15e65114c707d541a459d2
Aryaman Singh <151651851+Aryaman006@users.noreply.github.com>
2025-06-01 00:08:12 +0530

    first commit

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,3 +1,4 @@
+"use client"
 const shortToFullTeamNames = {
   RCB: "Royal Challengers Bangalore",
   CSK: "Chennai Super Kings",
--- src/components/SortSelect.jsx
+++ src/components/SortSelect.jsx
@@ -1,3 +1,4 @@
+"use client"
 export default function SortSelect({ sortBy, setSortBy }) {
   return (
     <div className="flex items-center gap-3">
